a2a3de1a1b41979660195b07bc8856aae128bf11
DigitalSaf <safwan.okomi@gmail.com>
2020-05-28 17:20:58 +0100

    neww

---
+++ script.js
@@ -0,0 +1,33 @@
+function addToWall() {
+    let txtM = document.getElementById("fname").value
+    fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall', {
+      method: 'POST',
+      body: JSON.stringify({
+        message: txtM
+      })
+    }).then(function(response) {
+      if (response.status == 200) {
+        console.log("Ok!")
+      } else {
+        console.log("Oh no!")
+      }
+    })
+}
+  
+  fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(response => response.json()).then(data => addElements(data))
+  
+  if (localStorage.getItem('test') == "key") {
+    console.log("It's there!")
+  } else {
+    localStorage.setItem('test', 'key')
+  }
+  
+  function addElements(list) {
+    list.forEach(function(element) {
+      let newLi = document.createElement("li")
+      let newContent = document.createTextNode(element)
+      newLi.appendChild(newContent)
+      let currentUl = document.getElementById("list")
+      document.body.insertBefore(newLi, currentUl)
+    })
+  }
